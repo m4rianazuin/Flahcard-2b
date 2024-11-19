ba5f0f26a40756900e9322442c84aca9ce0b24d9
Mariana <133695183+m4rianazuin@users.noreply.github.com>
2024-11-19 10:03:49 -0300

    Add files via upload

---
+++ continuação flashcard/continuação flashcard/perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro alvares cabral descobriu o Brasil'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro alvares cabral descobriu o Brasil'
+)
